8ce8d6a8a91cca519df3b3d280a5d30518582ffd
ANDREW SCHLIEWE <ajschliewe@rockwellautomation.com>
2019-11-09 13:27:52 -0600

    Changed the connection string to a localhost server versus an azure db and corrected the case to a parameter in a stored procedure

--- RaidManagementDB/usp_CreateScheduleEntry.sql
+++ RaidManagementDB/usp_CreateScheduleEntry.sql
@@ -2,7 +2,7 @@
 	@RaidId int,
 	@Date datetime2(0)
 AS
-	IF (Select count(*) from Schedule where TimeStamp=@date) = 0
+	IF (Select count(*) from Schedule where TimeStamp=@Date) = 0
 	BEGIN
 		Insert into Schedule(Description,TimeStamp,RaidId) values ('Raid Attendance',@Date,@RaidId)
 		Select ScheduleId from Schedule where TimeStamp = @Date
